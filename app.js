c7e8cf7b52b48b7d0243f6c8de954305e6caf73a
Henrique Esteves <henriesteves@gmail.com>
2017-11-25 22:36:32 -0200

    Add pages and admin pages routes

--- app.js
+++ app.js
@@ -24,11 +24,12 @@ app.set('view engine', 'ejs');
 // Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
- res.render('index', {
-   title: 'Home'
- });
-});
+// Set routes
+const pages = require('./routes/pages');
+const adminPages = require('./routes/adminPages');
+
+app.use('/', pages);
+app.use('/admin/pages', adminPages);
 
 // Start the server
 const port = 3000;

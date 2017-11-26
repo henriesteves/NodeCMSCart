8f6fe31e432f35b7179a8bacd4f73dcbe5e5a61e
Henrique Esteves <henriesteves@gmail.com>
2017-11-26 00:37:57 -0200

    Add form to admin pages

--- routes/adminPages.js
+++ routes/adminPages.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+/*
+* Get page index
+*/
 router.get('/', (req, res) => {
   res.send('Admin Area');
+});
+
+/*
+* Get add page
+*/
+router.get('/add-page', (req, res) => {
+  const title = '';
+  const slug = '';
+  const content = '';
+
+  res.render('admin/addPage', {
+    title: title,
+    slug: slug,
+    content: content
+  });
+
 });
 
 // Exports

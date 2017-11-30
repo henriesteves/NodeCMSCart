fc05f73501cf499388834ea29169691602c354a7
Henrique Esteves <henriesteves@gmail.com>
2017-11-30 10:49:00 -0200

    Add admin edit post route and product gallery route

--- routes/adminProducts.js
+++ routes/adminProducts.js
@@ -71,8 +71,6 @@ router.post('/add-product', (req, res) => {
 
   const errors = req.validationErrors();
 
-  console.log(errors)
-
   if (errors) {
     Category.find((err, categories) => {
       res.render('admin/addProduct', {
@@ -147,7 +145,7 @@ router.post('/add-product', (req, res) => {
 */
 router.get('/edit-product/:id', (req, res) => {
 
-  var errors;
+  let errors;
 
   if (req.session.errors) errors = req.session.errors;
   req.session.errors = null;
@@ -190,12 +188,102 @@ router.get('/edit-product/:id', (req, res) => {
 });
 
 /*
-* Post edit product
-*/
+ * POST edit product
+ */
 router.post('/edit-product/:id', (req, res) => {
 
-  
+  const imageFile = typeof req.files.image !== "undefined" ? req.files.image.name : "";
 
+  req.checkBody('title', 'Title must have a value.').notEmpty();
+  req.checkBody('description', 'Description must have a value.').notEmpty();
+  req.checkBody('price', 'Price must have a value.').isDecimal();
+  req.checkBody('image', 'You must upload an image').isImage(imageFile);
+
+  const title = req.body.title;
+  const slug = title.replace(/\s+/g, '-').toLowerCase();
+  const description = req.body.description;
+  const price = req.body.price;
+  const category = req.body.category;
+  const pimage = req.body.pimage;
+  const id = req.params.id;
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.session.errors = errors;
+    res.redirect('/admin/products/edit-product/' + id);
+  } else {
+    Product.findOne({ slug, _id: { '$ne': id } }, (err, product) => {
+      if (err)
+        console.log(err);
+
+      if (product) {
+        req.flash('danger', 'Product title exists, choose another.');
+        res.redirect('/admin/products/edit-product/' + id);
+      } else {
+        Product.findById(id, (err, product) => {
+          if (err)
+            console.log(err);
+
+          product.title = title;
+          product.slug = slug;
+          product.description = description;
+          product.price = parseFloat(price).toFixed(2);
+          product.category = category;
+          if (imageFile != "") {
+            product.image = imageFile;
+          }
+
+          product.save((err) => {
+            if (err)
+              console.log(err);
+
+            if (imageFile != "") {
+              if (pimage != "") {
+                fs.remove('public/product-images/' + id + '/' + pimage, (err) => {
+                  if (err)
+                    console.log(err);
+                });
+              }
+
+              const productImage = req.files.image;
+              const path = 'public/product-images/' + id + '/' + imageFile;
+
+              productImage.mv(path, (err) => {
+                return console.log(err);
+              });
+
+            }
+
+            req.flash('success', 'Product edited!');
+            res.redirect('/admin/products/edit-product/' + id);
+          });
+
+        });
+      }
+    });
+  }
+
+});
+
+/*
+* Post product gallery
+*/
+router.post('/product-gallery/:id', (req, res) => {
+  const productImage = req.files.file;
+  const id = req.params.id;
+  const path = 'public/product-images/' + id + '/gallery/' + req.files.file.name;
+  const thumbsPath = 'public/product-images/' + id + '/gallery/thumbs/' + req.files.file.name;
+
+  productImage.mv(path, (err) => {
+    if (err) console.log(err);
+
+    resizeImg(fs.readFileSync(path), { widht: 100, height: 100}).then((buf) => {
+      fs.writeFileSync(thumbsPath, buf);
+    });
+  });
+
+  res.sendStatus(200);
 });
 
 /*

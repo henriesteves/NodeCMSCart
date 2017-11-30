bd6a4665c3ae28558e5125d75795023bb15af950
Henrique Esteves <henriesteves@gmail.com>
2017-11-29 23:19:05 -0200

    Add Admin edit product form, route and Dropzone plugin

--- routes/adminProducts.js
+++ routes/adminProducts.js
@@ -41,12 +41,12 @@ router.get('/add-product', (req, res) => {
   const price = '';
 
   Category.find((err, categories) => {
-      res.render('admin/addProduct', {
-          categories,
-          title,
-          description,
-          price
-      });
+    res.render('admin/addProduct', {
+      categories,
+      title,
+      description,
+      price
+    });
   });
 
 });
@@ -84,17 +84,17 @@ router.post('/add-product', (req, res) => {
       });
     });
   } else {
-    Product.findOne({ slug }, (err, product) => {
+    Product.findOne({slug}, (err, product) => {
       if (product) {
         req.flash('danger', 'Product title exists, choose another.');
-          Category.find((err, categories) => {
-              res.render('admin/addProduct', {
-                  categories,
-                  title,
-                  description,
-                  price
-              });
+        Category.find((err, categories) => {
+          res.render('admin/addProduct', {
+            categories,
+            title,
+            description,
+            price
           });
+        });
       } else {
         const price2 = parseFloat(price).toFixed(2);
         const product = new Product({
@@ -106,21 +106,21 @@ router.post('/add-product', (req, res) => {
           image: imageFile
         });
 
-        product.save((err) =>{
+        product.save((err) => {
           if (err) {
             return console.log(err);
           }
 
-          mkdirp('public/product-images/' + product._id, (err) =>{
+          mkdirp('public/product-images/' + product._id, (err) => {
             return console.log(err);
           });
 
-          mkdirp('public/product-images/' + product._id + '/gallery', (err) =>{
-              return console.log(err);
+          mkdirp('public/product-images/' + product._id + '/gallery', (err) => {
+            return console.log(err);
           });
 
-          mkdirp('public/product-images/' + product._id + '/gallery/thumbs', (err) =>{
-              return console.log(err);
+          mkdirp('public/product-images/' + product._id + '/gallery/thumbs', (err) => {
+            return console.log(err);
           });
 
           if (imageFile != '') {
@@ -139,46 +139,52 @@ router.post('/add-product', (req, res) => {
       }
     });
   }
-  
-});
 
-/*
-* Post reorder product index
-*/
-router.post('/reorder-product', (req, res) => {
-  var ids = req.body['id[]'];
-  var count = 0;
-
-  for (var i = 0; i < ids.length; i++) {
-    var id = ids[i];
-    count++;
-
-    ((count) => {
-      Product.findById(id, (err, product) => {
-        product.sorting = count;
-        product.save((err) => {
-          if (err) return console.log(err)
-        });
-      });
-    })(count)
-    
-  }
 });
 
 /*
 * Get edit product
 */
 router.get('/edit-product/:id', (req, res) => {
-  
-  Product.findById(req.params.id, (err, product) => {
-    if (err) return console.log(err);
 
-    res.render('admin/editProduct', {
-      id: product._id,
-      title: product.title,
-      slug: product.slug,
-      content: product.content
+  var errors;
+
+  if (req.session.errors) errors = req.session.errors;
+  req.session.errors = null;
+
+  Category.find((err, categories) => {
+
+    Product.findById(req.params.id, (err, product) => {
+      if (err) {
+        console.log(err);
+        res.redirect('/admin/products');
+      } else {
+        const galleryDir = 'public/product-images/' + product.id + '/gallery';
+        let galleryImages = null;
+
+        fs.readdir(galleryDir, (err, files) => {
+          if (err) {
+            console.log(err);
+          } else {
+            galleryImages = files;
+
+            res.render('admin/editProduct', {
+              errors,
+              id: product.id,
+              categories,
+              category: product.category.replace(/\s+/g, '-').toLowerCase(),
+              title: product.title,
+              description: product.description,
+              price: parseFloat(product.price).toFixed(2),
+              image: product.image,
+              galleryImages
+            });
+          }
+        });
+      }
+
     });
+
   });
 
 });
@@ -188,58 +194,7 @@ router.get('/edit-product/:id', (req, res) => {
 */
 router.post('/edit-product/:id', (req, res) => {
 
-  req.checkBody('title', 'Title must have a value.').notEmpty();
-  req.checkBody('content', 'Content must have a value.').notEmpty();
-
-  const id = req.params.id;
-  let title = req.body.title;
-  let slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-  if (slug == '') slug = title.replace(/\s+/g, '-').toLowerCase();
-  let content = req.body.content;
-
-  const errors = req.validationErrors();
-
-  if (errors) {
-    res.render('admin/editProduct', {
-      errors,
-      id,
-      title,
-      slug,
-      content
-    });
-  } else {
-    Product.findOne({ slug, _id: {'$ne': id} }, (err, product) => {
-      if (product) {
-        req.flash('danger', 'Product slug exists, choose another.');
-        res.render('admin/editProduct', {
-          id,
-          title,
-          slug,
-          content
-        });
-      } else {
-        Product.findById(id, (err, product) => {
-          if (err) console.log(err);
-
-          product.title = title;
-          product.slug = slug;
-          product.content = content;
-
-          console.log(product)
-
-          product.save((err) => {
-            if (err) {
-              return console.log(err);
-            }
-
-            req.flash('success', 'Product edited!');
-            res.redirect('/admin/products/edit-product/' + id);
-          });
-        });
-
-      }
-    });
-  }
+  
 
 });
 

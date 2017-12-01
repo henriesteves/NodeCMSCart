22361a7d925c60f82149a04b6e34f3bcbe1fa731
Henrique Esteves <henriesteves@gmail.com>
2017-11-30 23:36:13 -0200

    Add content to pages and front pages route

--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
+// Get page model
+const Page = require('../models/page');
+
+/*
+ * Get
+ */
 router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Home'
+
+  Page.findOne({ slug: 'home' }, (err, page) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.render('index', {
+      title: page.title,
+      content: page.content
+    });
   });
+
+});
+
+/*
+ * Get a page
+ */
+router.get('/:slug', (req, res) => {
+
+  const slug = req.params.slug;
+
+  Page.findOne({ slug }, (err, page) => {
+    if (err) {
+      console.log(err);
+    }
+
+    if (!page) {
+      res.redirect('/');
+    } else {
+      res.render('index', {
+        title: page.title,
+        content: page.content
+      });
+    }
+  });
+
 });
 
 // Exports

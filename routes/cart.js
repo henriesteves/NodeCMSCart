8c36e89c0014317ac5808ebe37782d62b8352f1a
Henrique Esteves <henriesteves@gmail.com>
2018-01-15 21:38:31 -0200

    Add plus, minus, clear item and clear cart button features and routes

--- routes/cart.js
+++ routes/cart.js
@@ -20,7 +20,7 @@ router.get('/add/:product', (req, res) => {
 
       req.session.cart = [];
       req.session.cart.push({
-        title: slug,
+        title: product.title,
         qty: 1,
         price: parseFloat(product.price).toFixed(2),
         image: (product.image) ? '/product-images/' + product._id + '/' + product.image : '/images/noimage.png'
@@ -32,7 +32,7 @@ router.get('/add/:product', (req, res) => {
       var newItem = true;
 
       for (var i = 0; i < cart.length; i++) {
-        if (cart[i].title == slug) {
+        if (cart[i].title == product.title) {
           cart[i].qty++;
           newItem = false;
           break;
@@ -41,7 +41,7 @@ router.get('/add/:product', (req, res) => {
 
       if (newItem) {
         cart.push({
-          title: slug,
+          title: product.title,
           qty: 1,
           price: parseFloat(product.price).toFixed(2),
           image: (product.image) ? '/product-images/' + product._id + '/' + product.image : '/images/noimage.png'
@@ -64,10 +64,64 @@ router.get('/add/:product', (req, res) => {
  */
 router.get('/checkout', (req, res) => {
 
-  res.render('checkout', {
-    title: 'Ckeckout',
-    cart: req.session.cart
-  });
+  if (req.session.cart && req.session.cart.length == 0) {
+    delete req.session.cart;
+    res.redirect('/cart/checkout');
+  } else {
+    res.render('checkout', {
+      title: 'Ckeckout',
+      cart: req.session.cart
+    });
+  }
+
+});
+
+/*
+ * Get update product
+ */
+router.get('/update/:product', (req, res) => {
+
+  const slug = req.params.product;
+  const action = req.query.action;
+
+  var cart = req.session.cart;
+  
+  for(let i = 0; i < cart.length; i++) {
+    if (cart[i].title == slug) {
+      switch (action) {
+        case "add":
+          cart[i].qty++;
+          break;
+        case "remove":
+          cart[i].qty--;
+          if (cart[i].qty < 1) cart.splice(i, 1);
+          break;
+        case "clear":
+          cart.splice(i, 1);
+          if (cart.length == 0) delete req.session.cart;
+          break;
+        default:
+          console.log('Update problem')
+          break;
+      }
+      break;
+    }
+  }
+
+  req.flash('success', 'Cart updated!');
+  res.redirect('/cart/checkout');
+
+});
+
+/*
+ * Get clear cart
+ */
+router.get('/clear', (req, res) => {
+
+  delete req.session.cart;
+
+  req.flash('success', 'Cart cleared!');
+  res.redirect('/cart/checkout');
 
 });
 

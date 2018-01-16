d8bfb159975e30d4ec1fd1366b840dc5af03e07e
Henrique Esteves <henriesteves@gmail.com>
2018-01-16 21:32:41 -0200

    Add user register post route

--- routes/users.js
+++ routes/users.js
@@ -18,26 +18,77 @@ router.get('/register', (req, res) => {
 });
 
 /*
- * Get a page
+ * Post register
  */
-router.get('/:slug', (req, res) => {
+router.post('/register', (req, res) => {
 
-  const slug = req.params.slug;
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
 
-  Page.findOne({ slug }, (err, page) => {
-    if (err) {
-      console.log(err);
-    }
+  req.checkBody('name', 'Name is required!').notEmpty();
+  req.checkBody('email', 'Email is required!').isEmail();
+  req.checkBody('username', 'Username is required!').notEmpty();
+  req.checkBody('password', 'Password is required!').notEmpty();
+  req.checkBody('password2', 'Passwords do not match!').equals(password);
 
-    if (!page) {
-      res.redirect('/');
-    } else {
-      res.render('index', {
-        title: page.title,
-        content: page.content
-      });
-    }
-  });
+  const errors = req.validationErrors();
+
+  if (errors) {
+
+    res.render('register', {
+      errors,
+      title: 'Register'
+    });
+
+  } else {
+
+    User.findOne({ username }, (err, user) => {
+
+      if (err) {
+        console.log(err);
+      }
+
+      if (user) {
+
+        req.flash('danger', 'Username exists, choose another!');
+        res.redirect('/users/register');
+
+      } else {
+
+        var user = new User({
+          name,
+          email,
+          username,
+          password,
+          admin: 0
+
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+              console.log(err);
+            }
+
+            user.password = hash;
+
+            user.save((err) => {
+              if (err) {
+                console.log(err);
+              } else {
+                req.flash('success', 'You are now registered!');
+                res.redirect('/users/login')
+              }
+            });
+          });
+        });
+      }
+    });
+
+  }
 
 });
 
